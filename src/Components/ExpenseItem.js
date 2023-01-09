13a6172f34e2532eea22d7cb40b9a65ee7a1cb05
Sariqua <104137948+Sariqua@users.noreply.github.com>
2023-01-09 21:23:13 +0530

    kullu

---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,23 @@
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+
+const clickHandler = () =>{
+   console.log('clicked!!!');
+};
+
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Delete</button>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
---
+++ src/Components/NewExpenses/NewExpense.js
@@ -0,0 +1,14 @@
+
+import NewExpenseForm from './NewExpenseForm';
+import './NewExpense.css';
+
+const NewExpense =()=>{
+    return(
+            <div className="new-expense">
+                <NewExpenseForm/>
+
+            </div>
+    );
+
+}
+export default NewExpense;
